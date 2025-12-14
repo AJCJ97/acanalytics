5bfac796bfb412d4efe69a25fbe49c1972120072
ACastro <ajcastro9726@gmail.com>
2025-12-13 18:14:22 -0600

    updates

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'ACastroLab';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'ACAnalytics';
+export const SITE_DESCRIPTION = 'Collection of data analysis projects where I apply Python, SQL, visualization, and analytical thinking to solve problems and support business decisions';
